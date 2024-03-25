4e8dae906a017b539bb017e60118a499e2746b63
lilshebeary <cirijaye@gmail.com>
2024-03-25 14:29:04 -0700

    uploadImages multer

--- routes/uploadRouter.js
+++ routes/uploadRouter.js
@@ -7,25 +7,25 @@ const storage = multer.diskStorage({
         cb(null, 'public/images');
     },
     filename: (req, file, cb) => {
-        cb(null,  file.originalname)
+        cb(null, file.originalname)
     }
 });
 
 const imageFileFilter = (req, file, cb) => {
-    if(!file.originalname.match(/\.(jpeg|jpeg|png|gif)$/)) {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
         return cb(new Error('You can upload only image files!'), false);
     }
     cb(null, true);
 };
 
-const upload = multer({storage, fileFilter: imageFileFilter});
+const upload = multer({ storage: storage, fileFilter: imageFileFilter});
 
 const uploadRouter = express.Router();
 
 uploadRouter.route('/')
 .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
     res.statusCode = 403;
-    res.end('Get operation not supported on /imageUpload');
+    res.end('GET operation not supported on /imageUpload');
 })
 .post(authenticate.verifyUser, authenticate.verifyAdmin, upload.single('imageFile'), (req, res) => {
     res.statusCode = 200;
@@ -42,3 +42,4 @@ uploadRouter.route('/')
 });
 
 module.exports = uploadRouter;
+
