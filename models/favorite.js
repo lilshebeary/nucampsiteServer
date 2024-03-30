9f5ef8f677cf0da3027ca818893ff4b382db1604
lilshebeary <cirijaye@gmail.com>
2024-03-30 08:15:08 -0700

    add favorties model, route, use

---
+++ models/favorite.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Campsite = require('./campsite');
+const Schema = mongoose.Schema;
+
+
+const favoriteSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    campsites: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Campsite'
+    }]
+},
+{
+    timestamps: true
+})
+
+const Favorite = mongoose.model("Favorite", favoriteSchema)
+module.exports = Favorite;
